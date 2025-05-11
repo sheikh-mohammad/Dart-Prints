8424ee6a6bd623245ee4ff71a24597199e3acb3b
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-05-11 09:31:10 +0500

    Create q6.dart

---
+++ q6.dart
@@ -0,0 +1,8 @@
+void main() {
+  print("6. Make use of comments in you existing Dart & Flutter project. ");
+  print("7. Practice placement of print() statements in follwing sections of your project in assignment 6:");
+  print("      a. Head");
+  print("      b. Body (before your app's Dart')");
+  print("      c. Body (inside your app's Dart')");
+  print("      d. Body (after your app's Dart')");
+}
